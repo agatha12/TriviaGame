19f033d5410866a1264d406abc3598255d1acfdc
agatha12 <agkiecun@gmail.com>
2018-10-12 12:07:57 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,67 +5,117 @@ var selection = []
 var currentAnswer = []
 var currentQuestion = 0
 var score = 0
+var t
+var time = 30
+
 $(document).ready(function () {
     var start = $("<button>")
     start.addClass("startbutton")
     start.text("Start")
     start.css({ "margin-top": "100px", "width": "200px", "font-size": "20px" })
     $("#game").append(start)
+    $(".startbutton").on("click", game)
 
 })
 
-$(document).on("click", ".startbutton", game)
-function game() {
-    $("#game").empty()
-    currentAnswer = []
-    var q = $("<div>")
-    q.addClass("question")
-    q.text(question[currentQuestion])
-    $("#game").append(q)
 
-    var a = $("<form>")
-    a.addClass("answer")
-    $(".question").append(a)
-    for (i = 0; i < answer[currentQuestion].length; i++) {
-        var option = $("<input type=button>")
-        option.val(answer[currentQuestion][i])
-        if (currentAnswer.indexOf(option.val()) == -1) {
-            option.addClass("answerbutton")
-            $(".answer").append(option)
-            currentAnswer.push(option.val())
-            console.log(currentAnswer)
-        }
-        else {
-            console.log("repeat")
-        }
+function timer() {
+    $(".timer").empty()
+    $(".timer").prepend("You have " + time + " second left.")
+    time--
+
+    if (time === -1) {
+            var tUp = setTimeout(game, 3000)
+            displayGIf("assets/images/timesup.gif", "Times Up!")
     }
+}
 
-        $(".answerbutton").click(function checkanswer() {
+
+
+function game() {
+    clearInterval(t)
+    $("#game").empty()
+    if (currentQuestion === question.length) {
+        $("#game").html("Game Over! Your score is " + score + "/4")
+        var button = $("<button type=button>")
+        button.addClass("restartButton")
+        button.val("Restart Game")
+        $("#game").append(button)
+        $(".restartButton").on("click", reset)
+    }
+    else {
+        currentAnswer = []
+        time = 5
+        var left = $("<div>")
+        left.addClass("timer")
+        $("#game").prepend(left)
+        t = setInterval(timer, 1000)
+        var q = $("<div>")
+        q.addClass("question")
+        q.text(question[currentQuestion])
+        $("#game").append(q)
+
+        var a = $("<form>")
+        a.addClass("answer")
+        $(".question").append(a)
+        for (i = 0; i < answer[currentQuestion].length; i++) {
+            var option = $("<input type=button>")
+            option.val(answer[currentQuestion][i])
+            if (currentAnswer.indexOf(option.val()) == -1) {
+                option.addClass("answerbutton")
+                $(".answer").append(option)
+                currentAnswer.push(option.val())
+
+            }
+        }
+
+
+
+        $(".answerbutton").on("click", checkanswer)
+        function checkanswer() {
             var x = $(this).val()
-            console.log(x)
+            console.log(time)
+
 
             if (x === correct[currentQuestion]) {
                 score++
                 currentQuestion = currentQuestion + 1
-                console.log(currentQuestion)
-                alert("you're right")
                 game()
             }
             else {
-                currentQuestion = currentQuestion + 1
-                console.log(currentQuestion)
-                game()
+                var incorrect = setTimeout(game, 3000)
+                displayGIf("assets/images/wrong.gif", "That was not correct!")
+
+
             }
-
-
-
-        })
-
-
-        console.log(selection)
-
-    
-
-
+        }
+    }
 }
+
+function reset() {
+    selection = []
+    currentAnswer = []
+    currentQuestion = 0
+    score = 0
+    game()
+}
+
+function displayGIf(GIF, text){
+        currentQuestion = currentQuestion + 1
+        $("#game").empty()
+        var image = $("<img>")
+        image.attr("src", GIF)
+        image.addClass("gif")
+        var words = $("<p>")
+        words.text(text)
+        $("#game").append(text)
+        $("#game").append(image)
+}
+
+
+
+
+
+
+
 
